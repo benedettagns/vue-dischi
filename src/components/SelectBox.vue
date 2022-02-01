e7997e59ad8e4beccef5886c55973f2c5786a420
benedettagns <benedetta.genise01@gmail.com>
2022-02-01 16:13:23 +0100

    inizio

---
+++ src/components/SelectBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <select @change="$emit('select', selezioneGenere)" v-model="selezioneGenere">
+            <option value="">All</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="rock">Rock</option>
+            <option value="metal">Metal</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selezioneGenere= '',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
