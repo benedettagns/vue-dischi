33f9b35cf64bcc492087f3afb7a8f1da72d5190f
benedettagns <benedetta.genise01@gmail.com>
2022-02-01 16:29:02 +0100

    selezione genere

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <header-box/>
+    <select-box @select="selectGenre"/>
     <main-container :dischi="dischi"/>
   </div>
 </template>
@@ -9,23 +10,35 @@
 import HeaderBox from './components/HeaderBox.vue'
 import MainContainer from './components/MainContainer.vue'
 import axios from 'axios'
+import SelectBox from './components/SelectBox.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderBox,
-    MainContainer
+    MainContainer,
+    SelectBox
   }, 
   data() {
     return {
-      dischi: []
+      dischi: [],
+      genere: [],
     }
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
       this.dischi= response.data.response
+      this.genere= response.data.response
     })
-  }
+  },
+
+  methods:{
+    selectGenre(selezioneGenere) {
+      this.genere= this.dischi.filter((generi) => {
+        return generi.genre.includes(selezioneGenere)
+      })
+    }
+  } 
 }
 </script>
 
